cefa6966a93bcf4427fad126333239ff43e5bfb2
Martin Kuzdowicz <m.kuzdowicz@elsevier.com>
2017-12-01 15:00:57 +0000

    wip

--- single_layer_neural_network.py
+++ single_layer_neural_network.py
@@ -1,5 +1,6 @@
 from numpy import exp, array, random, dot
 
+
 class SingleLayerNeuralNetwork:
 
     def __init__(self):
@@ -7,10 +8,10 @@ class SingleLayerNeuralNetwork:
         self.synaptic_weights = 2 * random.random((3, 1)) - 1
 
     def __sigmoid(self, x):
-        return 1/(1 + exp(-x))
+        return 1 / (1 + exp(-x))
 
     def __sigmoid_derivative(self, x):
-        return x * (1-x)
+        return x * (1 - x)
 
     def train(self, training_set_inputs, training_set_outputs, number_of_iterations):
         for i in xrange(number_of_iterations):
@@ -21,23 +22,20 @@ class SingleLayerNeuralNetwork:
             print 'adjustment: {}'.format(adjustment)
             self.synaptic_weights = adjustment + self.synaptic_weights
 
-
     def predict(self, inputs):
         return self.__sigmoid(dot(inputs, self.synaptic_weights))
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     neural_network = SingleLayerNeuralNetwork()
 
     print 'Random starting synaptic weights: {}'.format(neural_network.synaptic_weights)
 
-    training_set_inputs = array([ [0,0,1], [1,1,1], [1,0,1], [0,1,1] ])
-    training_set_outputs = array([[0,1,1,0]])
+    training_set_inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
+    training_set_outputs = array([[0, 1, 1, 0]])
 
-    neural_network.train(training_set_inputs, training_set_outputs, number_of_iterations = 10000)
+    neural_network.train(training_set_inputs, training_set_outputs, number_of_iterations=10000)
 
     print 'New synaptic weights after training: {}'.format(neural_network.synaptic_weights)
 
-    print 'predicting: {}'.format(neural_network.predict(array([1,0,0])))
-
-
+    print 'predicting: {}'.format(neural_network.predict(array([1, 0, 0])))
